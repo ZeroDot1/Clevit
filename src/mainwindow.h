22839624427866e46f1433658468355ae51eccde
TigaxMT <thebigger018@protonmail.com>
2017-09-24 19:44:16 +0100

    * New menu Option - Theme: Choose a layout theme for the window * New menu Option - Options

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -45,6 +45,7 @@
 #include <QPrintDialog>
 #include <QImageReader>
 #include <QUrl>
+#include <QComboBox>
 
 namespace Ui {
 class MainWindow;
@@ -113,6 +114,16 @@ private slots:
 
     void on_clearBtn_clicked();
 
+    void on_actionWindow_Layout_Color_triggered();
+
+    void on_actionNo_Theme_triggered();
+
+    void on_actionWood_Theme_triggered();
+
+    void on_actionWave_Theme_triggered();
+
+    void on_actionRainbow_Theme_triggered();
+
 private:
     Ui::MainWindow *ui;
     About *about;
@@ -123,6 +134,7 @@ private:
     bool firstTime;
     bool cppOpened;
     bool canClear;
+
 
     QString path; // Path of text file
     QString text; // Text of text file
